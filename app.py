3ed4d49c5bd9115ac55f33c5d99b359211235f0a
NΞXØRΛ <help.socialearningbot@gmail.com>
2025-04-14 00:32:19 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from source import sys, time, __start__
+
+# Check if '--auth' argument is passed in the command line
+if '--auth' in sys.argv:
+    print (
+      f"\n{('-' * 23)}[\033[93mINFO\033[0m]{('-' * 23)}\n"
+       "A window will open shortly, Please log in to your social media account (\033[1mX/Twitter\033[0m) enabling the bot can perform tasks seamlessly\n"
+       "You can close the window properly by pressing \033[1mEnter\033[0m after finish logging in to your social media accounts\n"
+    )
+
+    time.sleep(5)
+
+if __name__ == '__main__':
+   __start__()
+
